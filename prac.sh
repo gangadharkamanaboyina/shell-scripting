96518a62fc950e92bcea8f47d08acfc390521041
Gangadhar <gangadharkamanaboyina@gmail.com>
2025-09-18 19:53:05 +0530

    Second commit

--- prac.sh
+++ prac.sh
@@ -4,9 +4,9 @@ PERSON1=Ram
 PERSON2=Sita
 
 echo "$PERSON1: Hello $PERSON2"
-echo "$PERSON2: HELLO $PERSON1"
-echo "$PERSON1: Ela vunnav $PERSON2"
+echo "$PERSON2: Hello $PERSON1"
+echo "$PERSON1: How are you $PERSON2"
 echo "$PERSON2: Good $PERSON1"
-echo "$PERSON1: Nee sangathi enti $PERSON2"
-echo "$PERSON2: Baganey Vunna $PERSON1"
+echo "$PERSON1: How about you $PERSON2"
+echo "$PERSON2: I am also doing good $PERSON1"
 
