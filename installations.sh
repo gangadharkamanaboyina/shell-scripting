d323ff508bd18446679c3bf4c60f39a159ae2f54
Gangadhar <gangadharkamanaboyina@gmail.com>
2025-09-22 20:30:14 +0530

    conditions

--- installations.sh
+++ installations.sh
@@ -4,13 +4,15 @@
 
 if  (($UID != 0)); then
    echo "Use root access" 
-   break
+   exit 1
 fi
 
 dnf install mysql -y
 
 if (($? == 0)); then
    echo "Successfully Installed MySql"
+   exit 0
 else
     echo "MySql Installation Failed"
+    exit 1
 fi
