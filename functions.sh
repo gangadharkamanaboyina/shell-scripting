fd264e730065b30453cb64276fada42b54607779
Gangadhar <gangadharkamanaboyina@gmail.com>
2025-09-22 20:42:54 +0530

    conditions

--- functions.sh
+++ functions.sh
@@ -2,5 +2,14 @@
 
 if ((UID != 0)); then
    echo "Use root privilages"
+   exit 1
 fi
 
+dnf install mysql -y
+
+if (($? != 0)); then
+   echo "Installation failed"
+   exit 1
+else
+   echo "Installation Success"
+fi
