7f7095b9b0292e4c86114ec1931af980cc237f4e
Gangadhar <gangadharkamanaboyina@gmail.com>
2025-09-22 20:47:14 +0530

    conditions

--- functions.sh
+++ functions.sh
@@ -7,9 +7,15 @@ fi
 
 dnf install mysql -y
 
+Status(){
 if (($? != 0)); then
    echo "Installation failed"
    exit 1
 else
    echo "Installation Success"
 fi
+}
+
+dnf install python3 -y
+
+Status()
