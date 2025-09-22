b452ab50973bcd5b257369487dcf5da99d2121ee
Gangadhar <gangadharkamanaboyina@gmail.com>
2025-09-22 22:06:44 +0530

    conditions

--- logs.sh
+++ logs.sh
@@ -3,3 +3,19 @@
 if ((UID != 0)); then
    echo "Need root privilages to run the script"
 fi
+
+Status(){
+    if (($1==0)); then
+        echo "$2 installed successfully"
+    else
+        echo "$2 installation failed"
+    fi
+}
+dnf list installed nginx
+
+   if(($?==0)); then
+       echo "Nginx already installed"
+    else
+       dnf install nginx -y
+       Status $? "Nginx"
+    fi
