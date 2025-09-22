ced905197029edcd0ca269b59e8e10c3e4768f13
Gangadhar <gangadharkamanaboyina@gmail.com>
2025-09-22 22:16:56 +0530

    conditions

--- logs.sh
+++ logs.sh
@@ -1,9 +1,13 @@
 #!/bin/bash
 
+
 if ((UID != 0)); then
    echo -e "\e[31m Need root privilages to run the script \e[0m"
    exit 1
 fi
+
+Log_Folder=$(mkdir /var/log/shell-scripting)
+Log_File=$($Log_Folder/install.log)
 
 Status(){
     if (($1==0)); then
@@ -12,7 +16,7 @@ Status(){
         echo -e "\e[31m $2 installation failed \e[0m"
     fi
 }
-dnf list installed nginx
+dnf list installed nginx $>$Log_File
 
    if(($?==0)); then
        echo -e " \e[33m Nginx already installed  \e[0m"
