51946342fe66c7eda0c971851de051c5f7a3b4ee
Gangadhar <gangadharkamanaboyina@gmail.com>
2025-09-22 19:32:56 +0530

    conditions

--- conditions.sh
+++ conditions.sh
@@ -2,10 +2,8 @@
 
 NUM=$1
 
-if [ $NUM -gt 10 ]; then
-   echo "$NUM is greater than 10"
-elif [ $NUM -eq 10 ]; then
-   echo "$NUM is equal to 10"
+if [ ($NUM % 2 -eq 0) ]; then
+   echo "$NUM is even"
 else 
-   echo "$NUM is less than 10"
+   echo "$NUM is odd"
 fi
