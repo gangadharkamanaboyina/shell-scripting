bb6806611ddf55eb907b66b2eb1c99a0a76d1693
Gangadhar <gangadharkamanaboyina@gmail.com>
2025-09-23 16:58:24 +0530

    Loops Practise

--- loops.sh
+++ loops.sh
@@ -5,6 +5,11 @@ G="\e[32m"
 W="\e[0m"
 Y="\e[33m"
 
+Log_Folder="/var/log/shell-prac"
+File_Name=$( echo $0 | cut -d "." -f1 )
+Log_File="$Log_Folder/$File_Name.log"
+
+mkdir -p $Log_Folder
 
 if((UID!=0)); then
     echo -e "$R Need root access to install $W"
@@ -17,16 +22,17 @@ Validate(){
        echo -e "$G $2 Installation Success $W"
     else
        echo -e "$R $2 Installation Failed $W"
+       exit 1
     fi
 }
 
 for package in $@; do
 
-   dnf list installed $package
+   dnf list installed $package &>>$File_Name
      if(($?!=0)); then
-   dnf install $package -y
+   dnf install $package -y &>>$File_Name
    Validate $? "$package"
      else
-       echo -e "$Y $package already installed $W"
+       echo -e "$Y $package already installed $W" 
      fi
 done
