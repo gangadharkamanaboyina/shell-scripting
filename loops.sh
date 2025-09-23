96a205d89c624b073c0f3132ba78b88d9c01e0bc
Gangadhar <gangadharkamanaboyina@gmail.com>
2025-09-23 15:41:31 +0530

    Loops Practise

--- loops.sh
+++ loops.sh
@@ -1,6 +1,12 @@
 #!/bin/bash
 
+R=$\e[31m
+G=$\e[32m
+W=$\e[0m
+Y=$\e[33m
+
+
 if((UID!=0)); then
-    echo -e "'/32m Need root access to install"
+    echo -e "$R Need root access to install $W"
 fi
 
